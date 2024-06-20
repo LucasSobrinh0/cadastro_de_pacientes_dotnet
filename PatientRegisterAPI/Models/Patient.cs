690461b08a3e7c183d3b62b90410f19453ebc3c2
lucassobrinh0 <lucas.sobrinho.crn@gmail.com>
2024-06-20 15:39:01 -0400

    first commit

---
+++ PatientRegisterAPI/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientRegisterAPI.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string Cpf {  get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; }
+    }
+}
